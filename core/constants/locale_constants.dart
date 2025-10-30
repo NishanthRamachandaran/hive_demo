b6a5f3ed5745d65f750520e401f8fa858835d833
Nishanth R <nishanthr2604@gmail.com>
2025-10-30 17:34:59 +0530

    updated

---
+++ core/constants/locale_constants.dart
@@ -0,0 +1,11 @@
+class LocaleConstants {
+  static const List<Map<String, String>> supportedLanguages = [
+    {'code': 'en', 'name': 'English'},
+    {'code': 'ar', 'name': 'العربية'},
+    {'code': 'hi', 'name': 'हिन्दी'},
+    {'code': 'ta', 'name': 'தமிழ்'},
+  ];
+
+  static const String defaultLanguage = 'en';
+  static const List<String> supportedLanguageCodes = ['en', 'ar', 'hi', 'ta'];
+}
